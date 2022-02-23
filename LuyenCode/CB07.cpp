148f83cba9189d451b28bf87271ff0494147b77b
Pupil <78217280+hocvn@users.noreply.github.com>
2022-02-23 16:25:45 +0700

    Create CB07.cpp

---
+++ LuyenCode/CB07.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <iomanip>
+using namespace std;
+
+int main(){
+
+    double cv,dt,r;
+    cin >> r;
+    cout << fixed << setprecision(3) << 2*3.14*r << " " << 3.14*r*r;
+    return 0;
+}
