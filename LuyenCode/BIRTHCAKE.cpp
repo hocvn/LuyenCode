6e7f0bee0e9f04096234113bdc6d8ab88688f67d
Pupil <78217280+hocvn@users.noreply.github.com>
2022-02-23 15:41:36 +0700

    Create BIRTHCAKE.cpp

---
+++ LuyenCode/BIRTHCAKE.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <queue>
+#define ll long long
+using namespace std;
+
+const int N = 1e5 + 3;
+int n,res;
+ll x,t,T;
+
+int main(){
+
+    priority_queue <ll> p;
+    ll sum = 0;
+    scanf("%d%lld",&n,&T);
+    for (int i = 1; i <= n; i++){
+        scanf("%lld%lld",&x,&t);
+        p.push(t);
+        sum += t;
+        while (!p.empty() && sum > T-x){
+            sum -= p.top();
+            p.pop();
+        }
+        res = max(res,(int)p.size());
+    }
+    cout << res;
+    return 0;
+}
