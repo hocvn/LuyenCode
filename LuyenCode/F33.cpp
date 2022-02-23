83ae95297f7c03c9630aa3d1d54b2c7d0dc3b3bc
Pupil <78217280+hocvn@users.noreply.github.com>
2022-02-23 15:31:33 +0700

    Update F33.cpp

--- LuyenCode/F33.cpp
+++ LuyenCode/F33.cpp
@@ -6,20 +6,20 @@ ll n;
 
 int main(){
 
-	cin >> n;
-	if (n & 1 == 0){
-		cout << -1;
-		return 0;
-	}
-	ll number = 3;
-	for (int i = 1; i <= 10000000; i++){
+    cin >> n;
+    if (n & 1 == 0){
+	    cout << -1;
+	    return 0;
+    }
+    ll number = 3;
+    for (int i = 1; i <= 10000000; i++){
 		if (number % n == 0){
-			cout << i;
-			return 0;
+	    	cout << i;
+	  	    return 0;
 		}
-		number = (number*10+3) % n;
-	}
-	cout << -1;
-	return 0;
+	    number = (number*10+3) % n;
+    }
+    cout << -1;
+    return 0;
 }
  
