268529073db2c8fb263cac0d94d6ac676f81e892
hocvn <78217280+hocvn@users.noreply.github.com>
2023-02-20 21:39:50 +0700

    Create CVB2H.py

---
+++ LuyenCode/CVB2H.py
@@ -0,0 +1,29 @@
+
+def convertToBase16(s) :
+	ret = ''
+	valueInBinary = 0
+
+	for i in range(len(n)) : 
+		
+		valueInBinary += (1 << (3 - (i % 4))) * int(s[i])
+	
+		if ((i+1) % 4 == 0) :
+			ret += base16[valueInBinary]
+
+			valueInBinary = 0
+
+	return str(ret)
+
+test = int(input())
+
+base16 = ['0','1','2','3','4','5','6','7','8','9','A','B','C','D','E','F']
+
+while test > 0 :
+	n = str(input())
+	
+	while (len(n) % 4 != 0) : 
+		n = '0' + n;
+	
+	print(convertToBase16(n))
+	
+	test -= 1
