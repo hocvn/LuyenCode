152566624f30e31e797b332f11556067dcc07fd0
Do Thai Hoc <78217280+hocvn@users.noreply.github.com>
2023-03-31 15:04:40 +0700

    Update CONCERT.cpp

--- LuyenCode/CONCERT.cpp
+++ LuyenCode/CONCERT.cpp
@@ -2,11 +2,55 @@
 #include <stack>
 using namespace std;
 
+#define N 100005
+int a[N], n;
+pair <int,int> st[N];
+
+int main() {
+
+    freopen("t.inp","r",stdin);
+    cin >> n;
+    for (int i = 0; i < n; i++) {
+        cin >> a[i];
+    }
+    long long res = 0;
+    int top = 0;
+
+    for (int i = 0; i < n; i++){
+        while (top != 0 && st[top].first < a[i]){
+            res += st[top].second;
+            top--;
+        }
+        if (top > 0){
+            if (st[top].first == a[i]){
+                res += st[top].second;
+            }
+            if (st[1].first > a[i]){
+                res++;
+            }
+        }
+        if (st[top].first == a[i]){
+            st[top].second++;
+        }
+        if (!top || st[top].first > a[i]){
+            st[++top] = {a[i], 1};
+        }
+    }
+
+    cout << res;
+    return 0;
+}
+
+/*
+#include <iostream>
+#include <stack>
+using namespace std;
+
 int a[100005], b[100005];
 int n;
 
 int main() {
-  
+
     cin >> n;
     a[0] = -1;
     for (int i = 1; i <= n; i++) {
@@ -35,3 +79,4 @@ int main() {
     cout << res;
     return 0;
 }
+*/
